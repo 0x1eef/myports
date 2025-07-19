71205a6055a6448f244ea5da2da07e95c33c12cc
0x1eef <0x1eef@protonmail.com>
2025-07-19 22:36:39 +0000

    www/code-server: fix port
    https://github.com/0x1eef/myports/pull/10 was a step in the right
    direction but it didn't cover everything, and the package that was
    built would install a broken copy of code-server.
    
    I decided to take a different approach. We now have the build/
    subdirectory with its own Makefile. It will produce a tarball that
    includes a fully built code-server. This would be done by a maintainer,
    and the tarball uploaded as a github reelease.
    
    The port Makefile in the parent directory then only downloads and
    extracts the contents of the earlier build.
    
    properly build vscode and code-server together. Beforehand - certain
    build artifacts were missing from the tarballs.

---
+++ freebsd/www/code-server/build/files/patch-lib_vscode_build_lib_extensions.js
@@ -0,0 +1,18 @@
+--- lib/vscode/build/lib/extensions.js.orig	2025-07-19 22:46:35 UTC
++++ lib/vscode/build/lib/extensions.js
+@@ -427,7 +427,7 @@ function doPackageLocalExtensionsStream(forWeb, disabl
+         const dependenciesSrc = productionDependencies.map(d => path_1.default.relative(root, d)).map(d => [`${d}/**`, `!${d}/**/{test,tests}/**`]).flat();
+         result = event_stream_1.default.merge(localExtensionsStream, gulp_1.default.src(dependenciesSrc, { base: '.' })
+             .pipe(util2.cleanNodeModules(path_1.default.join(root, 'build', '.moduleignore')))
+-            .pipe(util2.cleanNodeModules(path_1.default.join(root, 'build', `.moduleignore.${process.platform}`))));
++            .pipe(util2.cleanNodeModules(path_1.default.join(root, 'build', `.moduleignore.linux`))));
+     }
+     return (result
+         .pipe(util2.setExecutableBit(['**/*.sh'])));
+@@ -618,4 +618,4 @@ async function buildExtensionMedia(isWatch, outputRoot
+         outputRoot: outputRoot ? path_1.default.join(root, outputRoot, path_1.default.dirname(p)) : undefined
+     })));
+ }
+-//# sourceMappingURL=extensions.js.map
+\ No newline at end of file
++//# sourceMappingURL=extensions.js.map
